62c8a3a548817448fd5ad764c2c7e9118456dcc0
Iure Guimaraes <iure.guimaraes@ufba.br>
2022-03-08 20:15:55 -0300

    add axios api call

---
+++ src/services/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export interface Joke {
+  value: string
+}
+
+const api = axios.create({
+  baseURL: 'https://api.chucknorris.io/jokes/'
+});
+
+export function apiCallCategories(url: string) {
+  const data = api(url).then(response => {
+    return response.data;
+  });
+  console.log(data);
+  return data;
+}
+
+export function apiCallJoke(url: string) {
+  const data = api.get(url).then(response => {
+    return response.data;
+  });
+  console.log(data);
+  return data;
+}
