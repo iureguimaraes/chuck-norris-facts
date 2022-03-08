00d6989ad4a268ab565d9ce51b559c3ce0b8a3dc
Iure Guimaraes <iure.guimaraes@ufba.br>
2022-03-08 20:30:25 -0300

    list home jokes categories

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+import { apiCallCategories } from '../../services/api';
+
 const Home = () => {
+  const [categories, setCategories] = useState<string[]>([]);
+
+  async function getCategories() {
+    const dataRequest = apiCallCategories(`categories`);
+    console.log(dataRequest);
+    setCategories(await dataRequest);
+  }
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
-    <Link to="category/category">Category</Link>
+    <ul>
+      {categories?.map(category => (
+        <li key={category} className={category}>
+          <Link to={`category/${category}`}>{category}</Link>
+        </li>
+      ))}
+    </ul>
   );
 }
 
