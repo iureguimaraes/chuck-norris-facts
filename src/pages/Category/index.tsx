7039b110e161db9803fd067bc37767015d1744c7
Iure Guimaraes <iure.guimaraes@ufba.br>
2022-03-08 21:07:11 -0300

    load category joke

--- src/pages/Category/index.tsx
+++ src/pages/Category/index.tsx
@@ -1,8 +1,27 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+import { apiCallJoke, Joke } from '../../services/api';
 
 const Category = () => {
+  const { categoryName } = useParams();
+  const [joke, setJoke] = useState<Joke | null>({} as Joke);
+  
+  async function getJoke() {
+    const dataRequest = await apiCallJoke(`random?category=${categoryName}`);
+    setJoke(dataRequest);
+  }
+
+  useEffect(() => {
+    getJoke()
+  }, []);
+
   return (
-    <Link to="/">Home</Link>
+    <>
+      <h2>{joke?.value}</h2>
+      <button onClick = {getJoke}>Carregar outra</button>
+      <Link to='/'>Home</Link>
+    </>
   );
 }
 
